477982ca2950ab5aca1d9ee0d9eb0e14aebd92e4
Hayden Indiana Holdsworth <IndianaHoldsworth@gmail.com>
2021-12-15 18:04:34 -0700

    Adds link to homepage

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import BookList from './views/Books/BookList';
 import './App.css';
+import { BrowserRouter, NavLink, Route, Switch } from 'react-router-dom/cjs/react-router-dom.min';
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
       <h1>Library Catalog</h1>
-      <BookList />
+
+      <BrowserRouter>
+        <header>
+          <NavLink to="/books" data-testid="book-link">
+            Books
+          </NavLink>
+        </header>
+
+        <Switch>
+          <Route path="/books">
+            <BookList />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </main>
   );
 }
